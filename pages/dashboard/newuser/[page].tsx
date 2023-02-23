604ae63537ee8cb413c66ead3f7c2bad0d87cc8f
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-02-24 00:14:52 +0600

    prev error fix 1

--- pages/dashboard/newuser/[page].tsx
+++ pages/dashboard/newuser/[page].tsx
@@ -1,7 +1,7 @@
 import NewUserInitiation from "@/components/AuthComponents/NewUserInitiation/NewUserInitiation";
 import { useRouter } from "next/router";
 import React from "react";
-export default function page() {
+export default function Page() {
   const router = useRouter();
   const { page } = router.query;
   return <NewUserInitiation page={page} />;
