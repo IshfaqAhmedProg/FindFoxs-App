11320da0d9e548fbf9de1e610ca3ee49883e34f3
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-03-31 09:14:26 +0600

    changed favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           content="Best CRM for marketing professionals!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Logos/Favicon.svg" />
       </Head>
       <main className={styles.main}>
         <Cursor />
