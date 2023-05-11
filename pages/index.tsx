866b6312728358426d157c4bd4d27b32bf17e3b5
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-05-11 06:01:05 +0600

    check meta tag 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,9 @@ export default function Home() {
     <>
       <Head>
         <title>Scrapefox CRM</title>
+        <meta property="og:title" content="The Rock" />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://scrapfox.vercel.app/" />
         <meta property="og:image" content="/Logos/Favicon.svg" />
         <meta
           name="description"
