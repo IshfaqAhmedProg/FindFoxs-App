2b98cb22ebcf79d4627fc087b82b0f412a5c65a9
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-09-12 03:30:02 +0600

    feat:email validator inbox checker

--- pages/api/validators/validateEmail.ts
+++ pages/api/validators/validateEmail.ts
@@ -1,18 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
+const emailExistence = require("email-existence");
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   var headers = new Headers();
   headers.append("X-RapidAPI-Key", process.env.RAPID_API_KEY ?? "");
   headers.append("X-RapidAPI-Host", "mailcheck.p.rapidapi.com");
-
+  const email = req.body.domain;
   return new Promise<void>((resolve, reject) => {
-    fetch(`https://mailcheck.p.rapidapi.com/?domain=${req.body.domain}`, {
+    console.log(email);
+    fetch(`https://mailcheck.p.rapidapi.com/?domain=${email}`, {
       method: "GET",
       headers,
     })
       .then((response) => response.json())
-      .then((response) => {
-        res.status(200).json(response);
+      .then(async (response) => {
+        const updatedData = await checkEmailExistence(email, response);
+        res.status(200).json(updatedData);
         resolve();
       })
       .catch((error) => {
@@ -21,6 +23,31 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         res.status(405).end();
         resolve(); // in case something goes wrong in the catch block (as vijay commented)
       });
+  });
+};
+const checkEmailExistence = (email: string, obj: any) => {
+  return new Promise((resolve, reject) => {
+    emailExistence.check(
+      email,
+      (error: any, res: boolean) => {
+        const newCols = {
+          email,
+          inbox_checkable: false,
+          inbox_exists: false,
+        };
+        if (error) {
+          console.log(`Error from email-existence for ${email}`, error);
+          obj = { ...obj, ...newCols };
+        } else {
+          console.log(`Response from email-existence for ${email}`, res);
+          newCols.inbox_checkable = true;
+          newCols.inbox_exists = res;
+          obj = { ...obj, ...newCols };
+        }
+        resolve(obj);
+      },
+      1000
+    );
   });
 };
 export default handler;
